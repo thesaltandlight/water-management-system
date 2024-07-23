dd7f8694a75a100a6f9c33a21225dac2ed4ae6f3
ewurafua <ewurafuaquansah@gmail.com>
2024-07-24 02:10:08 +0930

    added explore button

--- wms/src/Pages/HomePage/Intro.jsx
+++ wms/src/Pages/HomePage/Intro.jsx
@@ -6,19 +6,19 @@ function Intro(){
                     <span className="wave">STAY</span> <span className="wave" style={{animationDelay: '0.1s'}}>HYDRATED,</span> <span className="wave">STAY</span> <span className="wave" style={{animationDelay: '0.1'}}>HEALTHY</span>
                 </h1>
                 <h2 className="text-3xl font-semibold font-serif mt-2 ml-10 text-[#24619D] fade-in-slide-up">Your Water Journey Starts Here</h2>
+                <button className="mt-7 ml-10 bg-[#24619D] text-white px-6 py-3 rounded-lg text-xl hover:bg-[#7BA5C9] transition-colors duration-300 fade-in-slide-up">Explore</button>
+
             </div>
+            
 
             <div className="flex-1 max-w-1/4 flex justify-center">
             <img src="/assets/Homepage.jpg" alt="Water Journey" className='w-3/4 h-auto max-w-md ml-56 fade-in-slide-up'></img>
             </div>
+
+
         </div>
     )
 }
 
-{/* <h1 className="text-5xl mb-1 font-serif font-black text-[#24619D] w-[700px] fade-in-slide-up">
-                <span className="wave">Hi!</span> <span className="wave" style={{ animationDelay: '0.1s' }}>Welcome</span> <span className="wave" style={{ animationDelay: '0.2s' }}>Back</span>
-            </h1>
-            <h3 className="text-2xl font-serif font-bold text-[#7BA5C9] mb-8 fade-in-slide-up">Stay Hydrated, Stay Healthy!</h3>
-            </div> */}
 
 export default Intro;
